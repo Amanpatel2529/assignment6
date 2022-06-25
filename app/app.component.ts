ff63f18822a4065fcbe6824e5c81f1be185f6ecc
Amanpatel2529 <106889345+Amanpatel2529@users.noreply.github.com>
2022-06-25 12:49:33 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: 
+  `
+  
+  <h3>
+  Marvellous Infosystems
+  </h3>
+
+<input type="text">  
+  
+  `
+ ,
+ styles:
+ [
+  ' input[type=text],select {border-color:blue; }' 
+ ] 
+
+})
+export class AppComponent {
+  title = 'assignment4';
+  Name = 'Marvellous';
+}
+
+
